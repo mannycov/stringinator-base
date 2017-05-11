515b6ee7d4396e6f890c4a35bb9791968f2d192c
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:42:46 -0700

    added tests for first function

--- test/underbar/first.test.js
+++ test/underbar/first.test.js
@@ -2,14 +2,19 @@ const _ = require('../../underbar');
 
 describe('first()', () => {
   it('returns the first element of an array', () => {
-    // Your code goes here
+    expect(_.first(['a', 'b', 'c'])).toEqual('a');
   });
 
   it('returns the first 2 elements of an array', () => {
-    // Your code goes here
+    expect(_.first(['a', 'b', 'c'], 2)).toEqual(['a', 'b']);
+  });
+
+  it('returns the first 3 elements of an array', () => {
+    expect(_.first([1, 2, 3, 4, 5], 3)).toEqual([1, 2, 3]);
   });
 
   it('returns the whole array if you ask for more elements than it has', () => {
-    // Your code goes here
+    expect(_.first(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+  
 });
