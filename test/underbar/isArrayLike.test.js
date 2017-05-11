ac5903f777a9fd984b9f023b133da3632ad4b46e
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:46:34 -0700

    added tests for isArrayLike function

--- test/underbar/isArrayLike.test.js
+++ test/underbar/isArrayLike.test.js
@@ -2,14 +2,26 @@ const _ = require('../../underbar');
 
 describe('isArrayLike()', () => {
   it('returns true for an actual array', () => {
-    // Your code goes here
+    expect(_.isArrayLike([1, 2, 3])).toBe(true);
+  });
+
+  it('returns false for a non-array', () => {
+    expect(_.isArrayLike(2, 2, 3)).toBe(false);
   });
 
   it('returns true for an array-like object', () => {
-    // Your code goes here
+    const arrayLikeObj = {
+      length: 10,
+      shape: 'square'
+    };
+    expect(_.isArrayLike(arrayLikeObj)).toBe(true);
   });
 
   it('returns false for a non-array-like object', () => {
-    // Your code goes here
+    const nonArrayLikeObj = {
+      'foo': 'bar'
+    };
+    expect(_.isArrayLike(nonArrayLikeObj)).toBe(false);
   });
+  
 });
