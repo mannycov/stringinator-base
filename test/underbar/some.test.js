7720b75c44fb340eb5c676f61eb638dc3d9cf143
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:32:22 -0700

    added tests for some function

--- test/underbar/some.test.js
+++ test/underbar/some.test.js
@@ -2,11 +2,18 @@ const _ = require('../../underbar');
 
 describe('some()', () => {
   it('returns true if any number is odd', () => {
-    // Your code goes here
+    const nums = [2, 4, 5, 6];
+    expect(_.some(nums, num => num % 2 === 1)).toBe(true);
   });
 
   it('returns false if no number is odd', () => {
-    // Your code goes here
+    const nums = [2, 4, 6, 8];
+    expect(_.some(nums, num => num % 2 === 1)).toBe(false);
+  });
+
+  it('returns true if any item is a number', () => {
+    const mixed = [true, 'string', 5, null];
+    expect(_.some(mixed, item => !isNaN(item))).toBe(true);
   });
 
 });
