5145e069706d80c2270b4accb7e2a27686ad369e
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:37:53 -0700

    added tests for every function

--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -3,15 +3,28 @@ const _ = require('../../underbar');
 describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
-      // Your code goes here
+      const nums = [1, 3, 5, 7];
+      expect(_.every(nums)).toBe(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const nums = [1, 3, 5, 7];
+      expect(_.every(nums, num => num % 2 === 1)).toBe(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
-      // Your code goes here
+      const nums = [1, 3, 5, 6, 7];
+      expect(_.every(nums, num => num % 2 === 1)).toBe(false);
+    });
+
+    it('returns true if all numbers in an array are greater than 1 and we test for such numbers', () => {
+      const nums = [2, 3, 4, 5];
+      expect(_.every(nums, num => num > 1)).toBe(true);
+    });
+
+    it('returns false if not all numbers in an array are greater than 1 and we test for such numbers', () => {
+      const nums = [1, 2, 3, 4, 5];
+      expect(_.every(nums, num => num > 1)).toBe(false);
     });
 
   });
