9a6420495a3d3632389fc5bc27e919962564cb90
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:31:33 -0700

    added tests for reject function

--- test/underbar/reject.test.js
+++ test/underbar/reject.test.js
@@ -2,10 +2,32 @@ const _ = require('../../underbar');
 
 describe('reject()', () => {
   it('rejects odd numbers from an array', () => {
-    // Your code goes here
+   const nums = [2, 4, 5, 6, 7, 8, 10, 11];
+    expect(_.reject(nums, num => num % 2 === 1)).toEqual([2, 4, 6, 8, 10]);
   });
 
   it('rejects null values from an object', () => {
-    // Your code goes here
+    const order = {
+      entree: 'burger',
+      side: null,
+      condiment: 'ketchup',
+      drink: null,
+      dessert: 'cookie'
+    };
+
+    const orderItems = _.reject(order, (value) => value === null);
+    expect(orderItems).toEqual(['burger', 'ketchup', 'cookie']);
   });
+
+  it('rejects number values from an object', () => {
+    const author = {
+      name: 'Jack Kerouac',
+      book: 'Dharma Bums',
+      age: 47
+    };
+
+    const personItems = _.reject(author, (value) => typeof(value) === 'number');
+    expect(personItems).toEqual(['Jack Kerouac', 'Dharma Bums']);
+  });
+  
 });
