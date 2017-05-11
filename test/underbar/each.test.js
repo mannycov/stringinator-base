460eb36e36aa8d3db583160dfe51e2a07312e301
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-11 16:36:21 -0700

    added tests for each function

--- test/underbar/each.test.js
+++ test/underbar/each.test.js
@@ -2,15 +2,57 @@ const _ = require('../../underbar');
 
 describe('each()', () => {
   it('iterates every element of an array, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+ const arr = ['a', 'b', 'c'];
+    let count = 0;
+    _.each(arr, function(element, index, array) {
+      expect(element).toEqual(array[index]);
+      count += 1;
+    });
+    expect(count).toBe(3);
   });
 
   it('iterates every element of an array-like object, passing that element, its corresponding index, and the entire array to the callback', () => {
-    // Your code goes here
+    const arrayLikeObj = {
+      length: 3,
+      1: 'a',
+      2: 'b',
+      3: 'c'
+    };
+    let count = 0;
+    _.each(arrayLikeObj, function(element, index, iteratedArrayLikeObj) {
+      expect(element).toEqual(iteratedArrayLikeObj[index]);
+      count += 1;
+    });
+    expect(count).toBe(3);
   });
 
   it('iterates every property of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
-    // Your code goes here
+    const obj = {
+      name: 'Calvin',
+      age: 6,
+      occupation: 'student'
+    };
+    let count = 0;
+    _.each(obj, function(value, key, iteratedObj) {
+      expect(value).toEqual(iteratedObj[key]);
+      count += 1;
+    });
+    expect(count).toBe(3);
+  });
+
+  it('iterates every key of an object, passing the value, the corresponding key, and the entire object to the callback', () => {
+    const obj2 = {
+      color: 'blue',
+      size: 4,
+      shape: 'rectangle',
+      flavor: null
+    };
+    let count = 0; 
+    _.each(obj2, function(value, key, iteratedObj) {
+      expect(value).toEqual(iteratedObj[key]);
+      count += 1;
+    });
+    expect(count).toBe(4);
   });
 });
 
